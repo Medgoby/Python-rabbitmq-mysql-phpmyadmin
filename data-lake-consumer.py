558188a46a2a3ced6bd4bd531f1effac9f23842d
Mohamed <71791410+Medgoby@users.noreply.github.com>
2023-04-20 09:08:09 +0200

    data lake and clean consumer

--- data-lake-consumer.py
+++ data-lake-consumer.py
@@ -35,25 +35,6 @@ def process_msg_lake(chan: BlockingChannel, method: Basic.Deliver, properties: B
         print(f"consuming --> {log}")
 
 
-
-# def process_msg_data_lake(chan: BlockingChannel, method: Basic.Deliver, properties: BasicProperties, body):
-
-#     body = body.decode("utf-8")
-#     #regex = re.compile(r"(?P<session>\S{1}|\S{15}) (?P<user>\S{1,50})")
-    
-#     if utils.get_user(body) != "-" and utils.get_session(body) != "-":
-#         row_logs = RowLog(id= utils.hash_body(body), timestamp=utils.get_timestamp(body), log=utils.get_log(body))
-#         connexion.add(row_logs)
-#         connexion.commit()
-#         print("Data inserted successfully")
-
-#         print(f"[{method.routing_key}] event consumed from exchange `{method.exchange}` body `{body}`")
-        
-#     else:
-#         print("Message ignored")
-
-
-
 channel.basic_consume(queue="queue-data-lake",
                       on_message_callback=process_msg_lake, auto_ack=True)
 channel.start_consuming()
